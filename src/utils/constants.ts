97c8e2784172a8e05215386df4999dcecdd2e52d
camfairchild <cameron@fairchild.dev>
2022-08-20 16:27:56 -0400

    fix endpoints to use wss

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,11 +11,11 @@ export const NETWORKS: SimpleProvider[] = [{
   id: "bittensor-nakamoto-mainnet",
   client: "Light client",
   endpoints: [
-    "wss://explorernode.nakamoto.opentensor.ai"
+    "wss://explorernode.nakamoto.opentensor.ai",
     //"ws://159.223.185.195:9944",
     //"ws://192.81.211.21:9944",
     //"ws://208.68.36.46:9944",
-    //"ws://subtensor.fairchild.dev:9944",
+    "wss://st.fairchild.dev:9944",
   ],
 }, {
   name: "Bittensor Nobunaga",
