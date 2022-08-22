14374a24d6e55e063c6a96a99b09cc4bdf7521a8
camfairchild <cameron@fairchild.dev>
2022-08-21 21:21:51 -0400

    Updates

--- hooks/api/useLocalStorage.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-export const useLocalStorage = (localStorageKey) => {
-    const [localValue, setLocalValue] = useState(localStorage.getItem(localStorageKey) || "");
-    useEffect(() => {
-        localStorage.setItem(localStorageKey, localValue);
-    }, [localValue, localStorageKey]);
-    return [localValue, setLocalValue];
-};
