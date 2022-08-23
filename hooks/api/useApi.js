9aafc5639ffbf3fe37d5d19f622f6d8dea2ee4cd
camfairchild <cameron@fairchild.dev>
2022-08-23 10:16:46 -0400

    Updates

--- hooks/api/useApi.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import "@polkadot/api-augment";
-import { ApiContext } from "../../utils/contexts";
-export const useApi = () => {
-    return useContext(ApiContext);
-};
