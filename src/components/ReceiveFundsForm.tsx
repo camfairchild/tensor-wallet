19eae615bc1fd3cd18e1f6f28fd2cd8d2b7c3cdb
Cameron Fairchild <cameron@opentensor.ai>
2023-01-18 10:16:36 -0500

    center QR

--- src/components/ReceiveFundsForm.tsx
+++ src/components/ReceiveFundsForm.tsx
@@ -6,16 +6,29 @@ import { AccountContext } from "../utils/contexts"
 import CopyAddressButton from "./CopyAddressButton"
 
 import ErrorBoundary from "./ErrorBoundary";
+import { Paper } from "@mui/material"
 
 const ReceiveFundsForm: FunctionComponent = () => {
   
   const { account } = useContext(AccountContext)
   return (
     <Stack direction="column" width={600} justifyContent="center" >
-      <QRCode value={account.accountAddress} includeMargin={true} size={400} style={{"justifySelf": "center"}} />
+      <Paper style={styles.qrParent} >
+        <QRCode value={account.accountAddress} includeMargin={true} size={400} style={{"justifySelf": "center"}} />
+      </Paper>
       <CopyAddressButton accountAddress={account.accountAddress} />
     </Stack>
   )
+}
+
+const styles = {
+  qrParent: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "1rem",
+    margin: "1rem",
+  },
 }
 
 export default ReceiveFundsForm
