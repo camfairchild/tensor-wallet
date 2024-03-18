0cc6de867fe76b888c7061937f4ff9670db73605
Cameron Fairchild <cameron@opentensor.ai>
2024-03-18 10:01:34 -0400

    update endpoint to OT lite node

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,20 +2,19 @@ import { SimpleProvider } from "./types"
 
 export const BURNR_WALLET = "tensor-wallet"
 
-export const POLKA_ACCOUNT_ENDPOINTS = {
-  polkadotjsexplorer: "polkadot.js.org/apps/?rpc=wss://entrypoint-finney.opentensor.ai:443",
-  scantensor: "https://scan.bittensor.com",
-  taostats: "https://x.taostats.io",
-}
-
-export const SS58_FORMAT = 42
-
 export const NETWORKS: SimpleProvider[] = [{
   name: "Bittensor Finney",
   id: "bittensor",
   client: "Sync Node",
   endpoints: [
-    "wss://entrypoint-finney.opentensor.ai:443",
+    "wss://lite.chain.opentensor.ai:443",
   ],
 }]
 
+export const POLKA_ACCOUNT_ENDPOINTS = {
+  polkadotjsexplorer: `polkadot.js.org/apps/?rpc=${NETWORKS[0].endpoints[0]}`,
+  scantensor: "https://scan.bittensor.com",
+  taostats: "https://x.taostats.io",
+}
+
+export const SS58_FORMAT = 42
